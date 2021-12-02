ea74b953b1117972cc0180e32c63599a3a691a9d
earth2ryan2earth <earth2ryan2earth@gmail.com>
2021-12-02 10:20:55 -0800

    jQuery .active on scroll

--- js/activeClass.js
+++ js/activeClass.js
@@ -1,4 +1,22 @@
-$(".nav-btn").click(function() {
-  $(".navbar ul li a").removeClass("active");
-  $(this).addClass("active");
+$(".nav-home, .nav-info, .nav-about").click(function() {
+  $("html, body").animate( {scrollTop: $('[id="' + $(this).attr("data-navtag") + '"]').offset().top - $(".navbar").outerHeight(true) }, 500);
+});
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() < $("#info").offset().top - $(".navbar").outerHeight(true) - 1 ){
+    $(".navbar a.active").removeClass("active");
+    $(".nav-home").addClass("active");
+  }
+  if ($(this).scrollTop() >= $("#info").offset().top - $(".navbar").outerHeight(true) - 1 ){
+    $(".navbar ul li a.active").removeClass("active");
+    $(".nav-info").addClass("active");
+  }
+  if ($(this).scrollTop() >= $("#about").offset().top - $(".navbar").outerHeight(true) - 1 ){
+    $(".navbar ul li a.active").removeClass("active");
+    $(".nav-about").addClass("active");
+  }
+});
+
+$(document).ready(function() {
+  $(".content #info, .content #about").css("scroll-margin-top", $(".navbar").outerHeight(true) );
 });
