bc543d02e2dce2599fc656e5d276ab03ea6d203f
earth2ryan2earth <earth2ryan2earth@gmail.com>
2021-12-02 13:09:38 -0800

    adjustHeight.js

--- js/adjustHeight.js
+++ js/adjustHeight.js
@@ -1,2 +1,3 @@
 $(".content #info, .content #about").css("scroll-margin-top", $(".navbar").outerHeight(true) );
+$(".content #info, .content #about").css("scroll-margin-top", "-=4.8em");
 $("head").append('<link rel="stylesheet" type="text/css" href="index.css">');
