143cbb3dfb259ff776d5412ceea574bdda2afc57
earth2ryan2earth <earth2ryan2earth@gmail.com>
2021-12-06 10:49:07 -0800

    Scroll margins

--- js/adjustHeight.js
+++ js/adjustHeight.js
@@ -1,3 +1,3 @@
 $(".content #info, .content #about").css("scroll-top", $(".navbar").outerHeight(true) );
-// $(".content #info, .content #about").css("scroll-margin-top", "-=4.8em");
+$(".content #info, .content #about").css("scroll-margin-top", "+=3.15em");
 $("head").append('<link rel="stylesheet" type="text/css" href="index.css">');
