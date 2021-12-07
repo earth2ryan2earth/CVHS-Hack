e64d7992956a4cc18e27b195581e70cce1778175
adamm-bash <92397480+adamm-bash@users.noreply.github.com>
2021-12-07 10:58:04 -0800

    Create activeClass.js

---
+++ js/activeClass.js
@@ -0,0 +1,18 @@
+$(".nav-home, .nav-info, .nav-about").click(function() {
+  $("html, body").animate( {scrollTop: $('[id="' + $(this).attr("data-navtag") + '"]').offset().top - $(".navbar").outerHeight(true) }, 500);
+});
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() < $("#info").offset().top - $(".navbar").outerHeight(true) - 1    ){
+    $(".navbar a.active").removeClass("active");
+    $(".nav-home").addClass("active");
+  }
+  if ($(this).scrollTop() >= $("#info").offset().top - $(".navbar").outerHeight(true) - 1 ){
+    $(".navbar ul li a.active").removeClass("active");
+    $(".nav-info").addClass("active");
+  }
+  if ($(this).scrollTop() >= $("#about").offset().top - $(".navbar").outerHeight(true) - 1 ){
+    $(".navbar ul li a.active").removeClass("active");
+    $(".nav-about").addClass("active");
+  }
+});
